507d9cb9d2d4cbcb909518aff1a315e9f2c6bf61
Brad Cornes <hello@bradley.dev>
2023-04-05 20:25:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# remark-rehype-wrap
+
+> Wrap `remark` or `rehype` subtrees in a new container node.
+
+## Installation
+
+```
+npm install remark-rehype-wrap
+```
+
+```js
+import { remark } from 'remark'
+import { remarkRehypeWrap } from 'remark-rehype-wrap'
+
+await remark().use(remarkRehypeWrap).process('<p>Hello, world!</p>')
+```
+
+## Usage
+
+See [test.js](/) for usage examples.
+
+## Options
+
+### `options.node`
+
+A new node to use as the wrapper. e.g. `{ type: 'element', tagName: 'div' }`
+
+### `options.start`
+
+Either:
+
+- `string`: A node selector. Nodes matching the selector create a new wrapper section.
+- `{ selector: string, inclusive?: boolean }`: A node selector, with optional `inclusive` option to specify whether the start node should be included in the new wrapper section (`true`, default) or not (`false`).
+
+### `options.end` (optional)
+
+Either:
+
+- `string`: A node selector. Nodes matching the selector end the current wrapper section.
+- `{ selector: string, inclusive?: boolean }`: A node selector, with optional `inclusive` option to specify whether the end node should be included in the current wrapper section (`true`) or not (`false`, default).
+
+If `end` is not defined then it is set to the same as `start`.
+
+### `options.transform` (optional)
+
+The `transform` function is passed each new wrapper node and should return a node. See [test.js](/) for an example.
---
+++ test.js
@@ -0,0 +1,91 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import remarkWrap from './index.js'
+import { remark } from 'remark'
+import { rehype } from 'rehype'
+import remarkMdx from 'remark-mdx'
+
+async function processMdx(string, ...options) {
+  return String(
+    await remark()
+      .use(remarkMdx)
+      .use(remarkWrap, ...options)
+      .process(string)
+  ).trim()
+}
+
+async function processRehype(string, ...options) {
+  return String(
+    await rehype()
+      .data('settings', { fragment: true })
+      .use(remarkWrap, ...options)
+      .process(string)
+  ).trim()
+}
+
+test('mdx', async () => {
+  let processed = await processMdx(`# Hello world`, {
+    node: { type: 'mdxJsxFlowElement', name: 'Foo' },
+    start: 'heading',
+  })
+  assert.equal(
+    processed,
+    `<Foo>
+  # Hello world
+</Foo>`
+  )
+})
+
+test('rehype', async () => {
+  let processed = await processRehype(`<h1>Hello world</h1>`, {
+    node: { type: 'element', tagName: 'div' },
+    start: 'element[tagName=h1]',
+  })
+  assert.equal(processed, '<div><h1>Hello world</h1></div>')
+})
+
+test('rehype nested', async () => {
+  let processed = await processRehype(
+    ['<h1>Hello world</h1>', '<p>Hello world</p>', '<p>Hello world</p>'].join('\n\n'),
+    {
+      node: { type: 'element', tagName: 'div' },
+      start: 'element[tagName=h1]',
+    },
+    {
+      node: { type: 'element', tagName: 'div' },
+      start: { selector: 'element[tagName=h1]', inclusive: false },
+    }
+  )
+  assert.equal(
+    processed,
+    ['<div><h1>Hello world</h1><div>', '<p>Hello world</p>', '<p>Hello world</p></div></div>'].join(
+      '\n\n'
+    )
+  )
+})
+
+// test('steal props', async () => {
+//   let processed = await processRehype(['<h1 id="intro">Hello world</h1>'].join('\n\n'), {
+//     node: { type: 'element', tagName: 'div' },
+//     start: 'element[tagName=h1]',
+//     props: 'element[tagName=h1]',
+//   })
+//   assert.equal(processed, ['<div id="intro"><h1>Hello world</h1></div>'].join('\n\n'))
+// })
+
+test('transform', async () => {
+  let processed = await processRehype(['<h1 id="intro">Hello world</h1>'].join('\n\n'), {
+    node: { type: 'element', tagName: 'div' },
+    start: 'element[tagName=h1]',
+    transform: (node) => {
+      // steal the properties from the `h1` and put them on the wrapper
+      let heading = node.children[0]
+      node.properties = heading.properties
+      heading.properties = {}
+      return node
+    },
+  })
+  assert.equal(processed, ['<div id="intro"><h1>Hello world</h1></div>'].join('\n\n'))
+})
+
+test.run()
